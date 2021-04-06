e3a9c22dda0c6f5161933c5c5e0a1250b7f999c4
clement-massit <clement.massit@gmail.com>
2021-04-06 10:41:29 +0200

    footer changes

--- view/footer.php
+++ view/footer.php
@@ -2,7 +2,10 @@
 </section>
 
 <footer>
-	IDU3 - Groupe 1 aka les king
+  <p class = "footer-text">
+    
+    Bienvenue sur le site de gestion d'entretien.</br> Vous trouverez tous les renseignements nécessaires sur la page de présentation.</br>Pour tout problème technique, veuillez contacter l'administrateur.
+  </p>
 </footer>
 
 </main>
