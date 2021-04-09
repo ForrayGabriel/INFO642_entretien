f7be08b2b62fb735779dea2ccbdd345f6dd85be0
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-04-09 16:02:25 +0200

    Update EventController add update

--- controller/EventController.php
+++ controller/EventController.php
@@ -8,29 +8,71 @@ class EventController extends Controller {
 
 	public function view() {
 		try {
-			$b = new Classroom(parameters()["id"]);
-			$this->render("view", $b);
+			$object = new Event(parameters()["id"]);
+			$this->render("view", array('object' => $object));
 		} catch (Exception $e) {
 			(new SiteController())->render("index");
 			// $this->render("error");
 		}
 	}
 
+	public function update_view(){
+		$object = new Event(parameters()["id"]);
+
+		foreach(Role::findAll() as $role){
+			if($role->name_role == "Etudiant"){
+				$role_ban = $role->idrole;
+			}
+		}
+
+		foreach(InternalUser::findAll() as $user){
+			if($user->idrole == $object->idevent_creator){
+				$user_event_creator = $user;
+			}
+		}
+
+
+		$this->render("update", array('object' => $object,'internaluser' => InternalUser::findAll(), 'role_ban' => $role_ban, 'actual_creator' => $user_event_creator));
+	}
+
+	public function update_event(){
+		if(isset(parameters()['idevent_creator']) and parameters()['idevent_creator']  != 0 and isset(parameters()['entitled_event']) and isset(parameters()['description_event']) and isset(parameters()['start_date']) and isset(parameters()['end_date']) and isset(parameters()['idevent'])){
+
+			$event = new Event(parameters()['idevent']);
+			$event->entitled_event = parameters()["entitled_event"];
+			$event->description_event = parameters()["description_event"];
+			$event->start_date = parameters()["start_date"];
+			$event->end_date = parameters()["end_date"];
+			$event->idevent_creator = parameters()['idevent_creator'];
+			
+			$event->update();
+		}
+		$this->render("index", array('event' => Event::findAll(),'classroom' => Classroom::findAll(), 'internaluser' => InternalUser::findAll()));
+
+	}
+
 	public function add_view(){
-		$this->render("add", array('event' => Event::findAll(),'classroom' => Classroom::findAll(), 'internaluser' => InternalUser::findAll()));
+		foreach(Role::findAll() as $role){
+			if($role->name_role == "Etudiant"){
+				$role_ban = $role->idrole;
+			}
+		}
+		$this->render("add", array('event' => Event::findAll(),'classroom' => Classroom::findAll(), 'internaluser' => InternalUser::findAll(), 'role_ban' => $role_ban));
 	}
 
 	public function add_event(){
-		if(isset(parameters()['entitled_event']) and isset(parameters()['description_event']) and isset(parameters()['start_date']) and isset(parameters()['end_date'])){
-			print("pute");
+		if(isset(parameters()['idevent_creator']) and parameters()['idevent_creator']  != 0 and isset(parameters()['entitled_event']) and isset(parameters()['description_event']) and isset(parameters()['start_date']) and isset(parameters()['end_date'])){
+
 			$event = new Event();
 			$event->entitled_event = parameters()["entitled_event"];
 			$event->description_event = parameters()["description_event"];
 			$event->start_date = parameters()["start_date"];
 			$event->end_date = parameters()["end_date"];
+			$event->idevent_creator = parameters()['idevent_creator'];
+			
 			$event->insert();
-			$this->render("index", Event::findAll());
 		}
+		$this->render("index", array('event' => Event::findAll(),'classroom' => Classroom::findAll(), 'internaluser' => InternalUser::findAll()));
 
 	}
 
