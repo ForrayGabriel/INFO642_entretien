10f3da54cd14abdc92c1131baeddaa1bdbede996
clement-massit <clement.massit@gmail.com>
2021-04-09 17:27:28 +0200

    presentation

--- view/site/presentation-container.php
+++ view/site/presentation-container.php
@@ -1,6 +1,1 @@
-
-
-
-
-
-<h1>zrazar</h1>
+<div><br><br><br></div><div><span style="white-space:pre">		</span><h1 class="texte_bienvenue">Bienvenue sur le site de gestion d'entretien.<br>Vous trouverez tous les renseignements nécessaires sur la page de présentation.<br>Pour tout problème technique, veuillez contacter l'administrateur. </h1></div>
