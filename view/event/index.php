4e3395f49574f5cc076e93c0de9ff314b0c95158
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-04-09 16:03:43 +0200

    Minor update Event index

--- view/event/index.php
+++ view/event/index.php
@@ -2,9 +2,9 @@
 <h2>Liste des event</h2>
 
 <?php
-print_r($data[0]);
 foreach($data['event'] as $event) {
-	echo "<a href='?r=event/view&id=".$event->idevent."'>".$event->idevent."</a>";
+	echo "<a href='?r=event/view&id=".$event->idevent."'>".$event->entitled_event."</a>";
+	echo "<br>";
 }
 
 ?>
