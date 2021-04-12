3e4357f40c71985552524bca303e081756ab6f31
clement-massit <clement.massit@gmail.com>
2021-04-12 18:52:54 +0200

    box shadow on block

--- view/site/student_view.php
+++ view/site/student_view.php
@@ -1,6 +1,6 @@
 
 <link rel="stylesheet" type="text/css" href="././css/student.css"/>
-<div id="photo"><img id="bande" src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"><div class="centered">DU TEXTE</div></div>
+<div id="photo"><img id="bande" src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"><div class="centered">Espace Etudiant</div></div>
 <div id="contenu">
 	<div id="bouttons">
 	<div id="boutton1">
