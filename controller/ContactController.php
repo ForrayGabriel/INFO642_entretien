b1ee9abb2d0cab0474ea24504792d27acb4c09fa
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-04-14 00:55:00 +0200

    Update ContactController.php

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -23,26 +23,22 @@ class ContactController extends Controller {
 		}
 	}
 
-	// public function send_response(){
-	// 	if (isset(parameters()['answer_iduser']) and isset(parameters()['answer_iduser_respondent']) and isset(parameters()['answer_idcontact']) and isset(parameters()['answer_title']) and isset(parameters()['answer_text'])){
-	// 		$responsecontact = new ResponseContact();
-	// 		$responsecontact->iduser = parameters()['answer_iduser_respondent'];
-	// 		$responsecontact->iduser_respondent = parameters()['answer_iduser'];
-	// 		$responsecontact->iduser_contact = parameters()['answer_idcontact'];
-	// 		$responsecontact->title_response = parameters()['answer_title'];
-	// 		$responsecontact->text_response = parameters()['answer_text'];
-	// 		$responsecontact->date_response = date("Y-m-d H:i:s");
-	// 		if (isset($_SESSION['user'])){
-	// 			if ($_SESSION['user']['idrole'] != 1) {
-	// 				$responsecontact->admin_response = 1;
-	// 			}
-	// 		}
-	// 		$responsecontact->insert();
+	public function send_response(){
+		if (isset(parameters()['answer_iduser_requestor']) and isset(parameters()['answer_iduser_receiver']) and isset(parameters()['answer_idcontact']) and isset(parameters()['answer_title']) and isset(parameters()['answer_text'])){
+			$responsecontact = new ResponseContact();
+			$responsecontact->idusercontact = parameters()['answer_idcontact'];
+			$responsecontact->iduser_requestor = parameters()['answer_iduser_requestor'];
+			$responsecontact->iduser_receiver = parameters()['answer_iduser_receiver'];
+			$responsecontact->title_response = parameters()['answer_title'];
+			$responsecontact->text_response = parameters()['answer_text'];
+			$responsecontact->date_response = date("Y-m-d H:i:s");
+			$responsecontact->admin_response = 1;
 
-	// 		$contact = new UserContact(parameters()['answer_idcontact']);
-	// 		$this->render("view", array('contact' => $contact, 'response' => ResponseContact::findAll(), 'internaluser' => InternalUser::findAll()));
+			$responsecontact->insert();
 
-	// 	}  
-	// }
+			$contact = new UserContact(parameters()['answer_idcontact']);
+			$this->render("view", array('contact' => $contact, 'response' => ResponseContact::findAll(), 'internaluser' => InternalUser::findAll()));
+		}  
+	}
 }
 
