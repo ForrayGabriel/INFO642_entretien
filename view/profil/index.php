81a98f021951d419cf2a458b48783d23ae20c98e
Souchon <72502457+romainsouchon@users.noreply.github.com>
2021-04-13 23:21:04 +0200

    1ere fin css

--- view/profil/index.php
+++ view/profil/index.php
@@ -1,15 +1,18 @@
-<link rel="stylesheet" type="text/css" href="././css/modif_profil.css"/>
+<script src="js/profil.js"></script>
 <h2>Information du profil</h2>
 
-<div class = "back">
-	<p>Rôle : Etudiant</p>
-	<p>Login : caullird</p>
-	<p>Numéro INE : 1445D8854</p>
-	<p>Numéro d'étudiant : 8985444447</p>
-	<p>Nom : CAULLIREAU</p>
-	<p>Prénom : Dorian</p>
+<div id = 'back'>
+	<div class = 'info'>
+		<p>Rôle : Etudiant</p>
+		<p>Login : caullird</p>
+		<p>Numéro INE : 1445D8854</p>
+		<p>Numéro d'étudiant : 8985444447</p>
+		<p>Nom : CAULLIREAU</p>
+		<p>Prénom : Dorian</p>
 
-	<p>Adresse mail : <input type="text" name="" disabled></p>
-	<p>Mot de passe : <input type="text" name="" disabled></p>
-
+		<p>Adresse mail : <input type="text" name="" disabled></p>
+		<p>Mot de passe : <input type="text" name="" disabled></p>
+		<button id = 'btn_update'>Modifier</button>
+		<button id = 'btn_valide'>Enregistrer</button>
+	</div>
 </div>
