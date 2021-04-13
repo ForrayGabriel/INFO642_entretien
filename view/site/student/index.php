37c3745a205fa2c319a87e06bd8097a9673891a8
clement-massit <clement.massit@gmail.com>
2021-04-14 00:39:41 +0200

    student home

--- view/site/student/index.php
+++ view/site/student/index.php
@@ -1,8 +1,16 @@
+
 <link rel="stylesheet" type="text/css" href="./././css/student_teacher.css"/>
 
 <div id="entretients">
 	Mes entretients
 	<div id="lesentretiens">
 		Test
+
+<link rel="stylesheet" type="text/css" href="././css/student_teacher.css"/>
+<div id="entretiens">
+	Mes entretiens
+	<div id="lesentretiens">Ici yaura les entretiens du étudiant
+
+
 	</div>
 </div>
