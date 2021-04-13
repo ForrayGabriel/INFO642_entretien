8540b9f23118033e708aeef88ed0e281fdbd2d51
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-04-13 22:03:37 +0200

    add header for student & teacher

---
+++ view/site/teacher/header.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" type="text/css" href="./././css/panel.css"/>
+
+<div id="photo">
+	<img id="bande" src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80">
+
+	<div class="centered">
+		Espace Enseignant
+	</div>
+
+</div>
+
+<div class="haut">
+	<a class="onglet" href=".">Accueil</a>
+	<a class="onglet" href='.?r=site/resultats'>Indiquer disponibilités</a>
+	<a class="onglet" href=''>Evaluer un évenement</a>
+</div>
+
+<div id="line"></div>
