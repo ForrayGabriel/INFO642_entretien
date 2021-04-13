7bdfdc1a7c3585a8cc4816e6ca8d52a5e20857e1
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-04-13 14:39:57 +0200

    update password

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -11,13 +11,39 @@ class LoginController extends Controller {
         $_SESSION["user"] = $internaluser;
         header('Location: .');
       } else {
-        // TODO add error
         $this->render("index", "error");
       }
     }
     
 		$this->render("index");
 	}
+
+  public function update(){
+
+    if(isset($_SESSION["user"]["username"])
+      && isset(parameters()["last_psw"])
+      && isset(parameters()["new_psw"])
+      && isset(parameters()["new_psw2"])) {
+        if (parameters()["new_psw"] != parameters()["last_psw2"]) {
+          $this->render("update", "error");
+          exit();
+        }
+
+        Internaluser::attempt($_SESSION["user"]["username"], parameters()["last_psw"]);
+        if (!$internaluser) {
+          $this->render("update", "error");
+          exit();
+        }
+
+        $internaluser["password"] = password_hash(parameters()["new_psw"]);
+        $this->render("update", "success");
+        exit();
+    }
+    else {
+      $this->index();
+    }
+    
+  }
 
   public function logout(){
     session_unset();
