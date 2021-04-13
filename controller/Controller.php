7715622dfe9bc6648213d52e5a434724d9eed4ee
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-04-13 22:02:16 +0200

    update include header

--- controller/Controller.php
+++ controller/Controller.php
@@ -19,6 +19,17 @@ class Controller {
 		$css = strtolower($model);
 
 		include_once "view/header.php";
+		if (isset($_SESSION['user'])){
+			if ($_SESSION['user']['idrole'] == 3) {
+				include_once 'view/site/admin/header.php';
+			}
+			if ($_SESSION['user']['idrole'] == 2) {
+				include_once 'view/site/teacher/header.php';
+			}
+			if ($_SESSION['user']['idrole'] == 1) {
+				include_once 'view/site/student/header.php';
+			}
+		}
 		include_once "view/".strtolower($model)."/".$view.".php";
 		include_once "view/footer.php";
 	}
