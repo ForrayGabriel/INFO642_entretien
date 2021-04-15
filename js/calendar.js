d9396fdf57073cfb10e75f809b78b3aeabf36b14
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-04-15 22:18:10 +0200

    alert when user can't change

--- js/calendar.js
+++ js/calendar.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    let colors = ["green", "orange", "red"];
+    let colors = ["green", "orange", "red", "neutral"]; // All colors
+    let colorsAccess = ["green", "red", "neutral"]; // Colors users can select or update
+    let colorsSwitch = ["green", "red"] // Colors switch when users click 
 
     document.querySelector(".calendar .content").addEventListener("click", function(event) {
         if (event.target.className != "day") return;
@@ -8,9 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
         focus_item = event.path[1].children[half_day == "AM" ? 1 : 2]
         
         actual_color = focus_item.classList[1]
-        next_color = colors[(Object.keys(colors).find(key => colors[key] === actual_color)+1)%colors.length];
-        event.path[1].children[half_day == "AM" ? 1 : 2].classList.remove(...colors);
-        event.path[1].children[half_day == "AM" ? 1 : 2].classList.add(next_color);
+        if (colorsAccess.includes(actual_color)) {
+            index_nextColor = parseInt(Object.keys(colorsAccess).find(key => colorsAccess[key] === actual_color))+1
+            console.log(index_nextColor+1)
+            next_color = colorsSwitch[index_nextColor%colorsSwitch.length];
+            console.log(next_color)
+            event.path[1].children[half_day == "AM" ? 1 : 2].classList.remove(...colors);
+            event.path[1].children[half_day == "AM" ? 1 : 2].classList.add(next_color);
+        } else {
+            alert("Tu as déja été affecté, contact l'admin si tu n'es pas disponible")
+        }
      });
 });
 
