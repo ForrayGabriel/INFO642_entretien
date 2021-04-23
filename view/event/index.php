c5a2dc296b4bafc00736a9c5e5a296d7bc9b8e2f
Souchon <72502457+romainsouchon@users.noreply.github.com>
2021-04-23 08:49:46 +0200

    debut visus event

--- view/event/index.php
+++ view/event/index.php
@@ -1,12 +1,16 @@
 
-<h2>Liste des event</h2>
-
-<?php
-foreach($data['event'] as $event) {
-	echo "<a href='?r=event/view&id=".$event->idevent."'>".$event->entitled_event."</a>";
-	echo "<br>";
-}
-
-?>
-<br><br><br>
-<a href='?r=event/add_view'>Ajouter un event</a>
+<h2>Liste des évenements</h2>
+<div class='back'>
+	<ul>
+		<?php
+		foreach($data['event'] as $event) {
+			echo"<li>";
+			echo "<a href='?r=event/view&id=".$event->idevent."'>".$event->entitled_event."</a>";
+			echo"</li>";
+		}
+		?>
+	</ul>
+	<div class="ajout">
+		<a href='?r=event/add_view'>Ajouter un nouvel évenement</a>
+	</div>
+</div>
