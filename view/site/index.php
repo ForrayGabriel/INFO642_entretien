845c268780759149babe69440b6c29f4e707f200
clement-massit <clement.massit@gmail.com>
2021-04-27 15:51:36 +0200

    presentation changes

--- view/site/index.php
+++ view/site/index.php
@@ -5,6 +5,17 @@
 <div id="presentation-container">
 	<h1 class="texte_bienvenue">Bienvenue sur le site de gestion d'entretien.<br>Vous trouverez tous les renseignements nécessaires sur la page de présentation.<br>Pour tout problème technique, veuillez contacter l'administrateur. </h1>
 </div>
+<p id="description">
+Chaque année, des entretiens sont organisés au sein de Polytech Annecy-Chambéry.<br> Ils concernent :
+</p>
+<div>
+	<ul id="liste-entretiens">
+		<li>Les entretiens de recrutements (PEIP ou filière ingénieurs)</li>
+		<li>Les évaluations de compétences des étudiants dans le cadre des APP</li>
+		<li>Les évaluations de projets (projet usage, recherche & développement,...)</li>
+		<li>Les évaluations de stage (stage ingénieur, ...)</li>
+	</ul>
+</div>
 
 <?php if (isset($_SESSION['user']) && $_SESSION['user']['idrole'] == 3){?>
 
---
+++ view/site/presentation-container.php
@@ -0,0 +1,2 @@
+
+	<h1 class="texte_bienvenue">Bienvenue sur le site de gestion d'entretien.<br>Vous trouverez tous les renseignements nécessaires sur la page de présentation.<br>Pour tout problème technique, veuillez contacter l'administrateur. </h1>
