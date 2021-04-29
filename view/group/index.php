6a48e9cefa3740c7db389d3c2ee4f9800ae49320
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-04-29 19:17:11 +0200

    add form for group people

---
+++ view/group/index.php
@@ -0,0 +1,8 @@
+
+
+<form action="?r=group/send" method="post" enctype="multipart/form-data">
+	<p>Images:
+		<input type="file" name="users" />
+		<input type="submit" value="Send" />
+	</p>
+</form>
