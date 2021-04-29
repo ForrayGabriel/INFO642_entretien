1996615e56ef4487f0671d5fc1f4beb5bcbd23c8
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-04-29 21:05:22 +0200

    add checkbox for group

--- view/group/index.php
+++ view/group/index.php
@@ -1,8 +1,39 @@
 
+<!-- BESOIN D'UNE EQUIPE DE CSS -->
 
+
+<center>
 <form action="?r=group/send" method="post" enctype="multipart/form-data">
-	<p>Images:
+	
+		<br>
+		<br>
+
+		<p>Fichier CSV : </p>
+
 		<input type="file" name="users" />
-		<input type="submit" value="Send" />
+
+		<br>
+		<br>
+
+		<?php 
+			foreach($data['groups'] as $group){
+				echo "<div>";
+				echo "<input type='checkbox' name='group_id[]' value='$group->idpeoplegroup'>";
+				echo "<label for='huey'>$group->title_peoplegroup</label>";
+				echo "</div>";
+			}
+
+		?>
+
+		<br>
+
+		<input type="submit" value="Importer" />
 	</p>
 </form>
+
+
+
+</center>
+
+
+
