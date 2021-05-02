ff6ec87a6c571043bb6efe984405ea7742ace0bb
clement-massit <clement.massit@gmail.com>
2021-05-02 18:29:41 +0200

    teacher prestation form

--- view/prestation/teacher.php
+++ view/prestation/teacher.php
@@ -1,15 +1,26 @@
+<link rel="stylesheet" type="text/css" href="././css/student_teacher.css"/>
 <img class="background-image" src="https://www.polytech.univ-smb.fr/fileadmin/_processed_/d/b/csm_Polytech_site_Annecy_vu_du_ciel_db27e8c54f.jpg">
 
+<p class="phrase">Bienvenue
 <?php 
-    $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date");
+print_r($_SESSION["user"]["prenom"]." ");
+print_r($_SESSION["user"]["nom"]); 
+?>
+</p>
+
+<?php 
+    $table_title = "Évènements à venir";
+    $table_header = array("Evenement","Date", "Jury", "Commentaire jury", "Salle");
 
     foreach ($data as &$prestation) {
         $table_content[$prestation->idprestation] = array(
             "Evenement" => $prestation->idevent->entitled_event,
-            "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
-            "Salle" => $prestation->idjury->idclassroom->name_classroom,
+            "Date" => $prestation->date_prestation,
             "Jury" => $prestation->idjury->name,
-            "Date" => $prestation->date_prestation
+            "Commentaire jury" => $prestation->comment_jury,
+            "Salle" => $prestation->idjury->idclassroom->name_classroom,
+            
+            
         );
     }
 
