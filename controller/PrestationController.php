974a4bba5c4f675216cfed668e83d73de7f1cb8b
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-05-04 01:54:59 +0200

    Update PrestationController.php

--- controller/PrestationController.php
+++ controller/PrestationController.php
@@ -7,169 +7,91 @@
             $user = new InternalUser($_SESSION["user"]["idinternaluser"]);
             if (is_student()) {
                 $student = Student::findOne(["idinternaluser" => $user->idinternaluser])[0];
-                $this->student($student, "comming");
-            } else if (is_teacher()) {
-                $this->teacher($user, "comming");
+                $this->student($student);
+            } else if (is_teacher() || is_admin()) {
+                $this->teacher($user);
             } else {
                 go_back();
             }
         }
 
-        public function historique() {
-            $user = new InternalUser($_SESSION["user"]["idinternaluser"]);
-            if (is_student()) {
-                header('Location: ?r=prestation/resultat');
-            } else if (is_teacher()) {
-                $this->teacher($user, "history");
-            } else {
-                print("TODO");
-                die();
+        // STUDENT 
+
+        public function student($student) {
+            $prestations = Prestation::findOne(["idstudent" => $student->idinternaluser->idinternaluser]);
+
+            $prestations = array_filter($prestations, function($prestation) {
+                return strtotime($prestation->date_prestation) > strtotime(date("Y-m-d H:i:s"));
+            });
+
+            $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date");
+
+            $table_content = array();
+            foreach ($prestations as &$prestation) {
+                $table_content[$prestation->idprestation] = array(
+                    "Evenement" => $prestation->idevent->entitled_event,
+                    "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
+                    "Salle" => $prestation->idjury->idclassroom->name_classroom,
+                    "Jury" => $prestation->idjury->name_jury,
+                    "Date" => date_format(date_create($prestation->date_prestation),'d/m/y'). " de " . $prestation->start_time . " à " . $prestation->end_time
+                );
             }
+
+            $this->renderComponent("table", ["header" => $table_header, "content" => $table_content]);
         }
 
-        public function resultat() {
-            $user = new InternalUser($_SESSION["user"]["idinternaluser"]);
-            $this->student($user, "noted");
-        }
-
-        public function notation() {
-            $user = new InternalUser($_SESSION["user"]["idinternaluser"]);
-            $this->teacher($user, "wait_notation");
-        }
-
-        public function admin($user) {
-
-        }
-
-        public function student($student, $state) {
-            print_r($student);
-            $prestations = Prestation::findOne(["idstudent" => $student->idinternaluser]);
-            if ($state == "comming") {
-                $prestations = array_filter($prestations, function($prestation) {
-                    return strtotime($prestation->date_prestation) > strtotime(date("Y-m-d H:i:s"));
-                });
-
-                    $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date");
-
-                    $table_content = array();
-                    foreach ($prestations as &$prestation) {
-                        $table_content[$prestation->idprestation] = array(
-                            "Evenement" => $prestation->idevent->entitled_event,
-                            "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
-                            "Salle" => $prestation->idjury->idclassroom->name_classroom,
-                            "Jury" => $prestation->idjury->name,
-                            "Date" => $prestation->date_prestation
-                        );
-                    }
-            } else if ($state == "noted") {
-                $prestations = array_filter($prestations, function($prestation) {
-                    return strtotime($prestation->date_prestation) < strtotime(date("Y-m-d H:i:s"));
-                });
-
-                $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date");
-
-                $table_content = array();
-                foreach ($prestations as &$prestation) {
-                    $table_content[$prestation->idprestation] = array(
-                        "Evenement" => $prestation->idevent->entitled_event,
-                        "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
-                        "Salle" => $prestation->idjury->idclassroom->name_classroom,
-                        "Jury" => $prestation->idjury->name,
-                        "Date" => $prestation->date_prestation
-                    );
-                }
-            }
-            $this->renderComponent("table", ["header"=>$table_header, "content"=>$table_content]);
-        }
-
-        public function teacher($user, $state) {
+        public function teacher($user) {
             
-            $table_content = array();$prestations = $user->getEnseignantPrestations();
+            $table_content = array();
+            $prestations = $user->getEnseignantPrestations();
             foreach ($prestations as $key => $prestation) {
                 $prestations[$key] = new Prestation($prestation["idprestation"]);
             }
-            if ($state == "comming") {
-                $prestations = array_filter($prestations, function($prestation) {
-                    return strtotime($prestation->date_prestation) > strtotime(date("Y-m-d H:i:s"));
-                });
 
-                $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date");
+            $prestations = array_filter($prestations, function($prestation) {
+                return strtotime($prestation->date_prestation) > strtotime(date("Y-m-d H:i:s"));
+            });
 
-                $table_content = array();
-                foreach ($prestations as &$prestation) {
-                    $table_content[$prestation->idprestation] = array(
-                        "Evenement" => $prestation->idevent->entitled_event,
-                        "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
-                        "Salle" => $prestation->idjury->idclassroom->name_classroom,
-                        "Jury" => $prestation->idjury->name,
-                        "Date" => $prestation->date_prestation
-                    );
-                }
-            
-                $this->renderComponent("table", ["header"=>$table_header, "content"=>$table_content]);
+            $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date");
 
-            } else if ($state == "wait_notation") {
-                print("Même page que Historique pour le moment car la gestions des notes n'est pas encore codé");
-                $prestations = array_filter($prestations, function($prestation) {
-                    return strtotime($prestation->date_prestation) < strtotime(date("Y-m-d H:i:s"));
-                });
-
-                $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date","Note");
-
-                $table_content = array();
-                foreach ($prestations as &$prestation) {
-                    $table_content[$prestation->idprestation] = array(
-                        "Evenement" => $prestation->idevent->entitled_event,
-                        "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
-                        "Salle" => $prestation->idjury->idclassroom->name_classroom,
-                        "Jury" => $prestation->idjury->name,
-                        "Date" => $prestation->date_prestation,
-                        "Note" => "TODO"
-                    );
-                }
-            
-                $this->renderComponent("table", ["header"=>$table_header, "content"=>$table_content]);
-            } else if ($state == "history") {
-                print("Même page que Notations pour le moment car la gestions des notes n'est pas encore codé");
-                $prestations = array_filter($prestations, function($prestation) {
-                    return strtotime($prestation->date_prestation) < strtotime(date("Y-m-d H:i:s"));
-                });
-
-                $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date","Note");
-
-                $table_content = array();
-                foreach ($prestations as &$prestation) {
-                    $table_content[$prestation->idprestation] = array(
-                        "Evenement" => $prestation->idevent->entitled_event,
-                        "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
-                        "Salle" => $prestation->idjury->idclassroom->name_classroom,
-                        "Jury" => $prestation->idjury->name,
-                        "Date" => $prestation->date_prestation,
-                        "Note" => "TODO"
-                    );
-                }
-            
-                $this->renderComponent("table", ["header"=>$table_header, "content"=>$table_content]);
+            $table_content = array();
+            foreach ($prestations as &$prestation) {
+                $table_content[$prestation->idprestation] = array(
+                    "Evenement" => $prestation->idevent->entitled_event,
+                    "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
+                    "Salle" => $prestation->idjury->idclassroom->name_classroom,
+                    "Jury" => $prestation->idjury->name_jury,
+                    "Date" => date_format(date_create($prestation->date_prestation),'d/m/y'). " de " . $prestation->start_time . " à " . $prestation->end_time
+                );
             }
 
+            $table_actions = array(
+                array("url" => "?r=prestation/notation&id=:id", "desc" => "", "icon" => "updatepasswordicon.png")
+            );
+        
+            $this->renderComponent("table", ["header" => $table_header, "content" => $table_content, "actions" => $table_actions]);    
         }
 
-        // public function add() {
-        //     if (isset(parameters()["idindividualevaluation"])) {
-        //         $prestation = new Prestation();
-        //         $prestation->idcompose = parameters()["idcompose"];
-        //         $prestation->idevaluationcriteria = parameters()["idevaluationcriteria"];
-        //         $prestation->idprestation = parameters()["idprestation"];
-        //         $prestation->individual_comment = parameters()["individual_comment"];
-        //         $prestation->individual_note = parameters()["individual_note"];
-        //         $prestation->insert();
-        //         $this->render("index", Classroom::findAll());
-        //     } else {
-        //         $prestation = new Prestation(parameters()["id"]);
-        //         $event = Evaluationcriteria::findOne(["idevent"=>$prestation->idprestation]);
-        //         $this->render("add", array('prestation'=>$prestation, 'evalcritere'=>$event));
-        //     }
-        // }
+        public function notation(){
+            $prestation = new Prestation(parameters()['id']);
+            $event = new Event($prestation->idprestation);
+            $evaluations_criterias = EvaluationCriteria::findOne(['idevent' => $event->idevent]);
+
+
+            $object = new Prestation();
+            $form_title = "Noter une prestation";
+            $form_content = array(
+                "Nom du critère" => array("type" => "text"),
+                "Echelle du critère" => array("type" => "text")
+            );
+
+            foreach($evaluations_criterias as $criteria){
+                    //TODO
+            }
+
+            $this->renderComponent("form", ["title"=>$form_title, "content"=>$form_content]);
+
+        }
 
     }
 
