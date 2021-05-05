22874624249572be114c34a5e5d60922f65f08f9
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-05-05 22:20:08 +0200

    Update ResultatController.php

--- controller/ResultatController.php
+++ controller/ResultatController.php
@@ -5,31 +5,46 @@ class ResultatController extends Controller {
 	var $rolepermissions = [1,2,3];
 
 	public function index() {
-
-		$prestations = Prestation::findOne(['idstudent' => $_SESSION['user']['idinternaluser']]);
-		$events = array();
-		$jurys = array();
-
-		foreach($prestations as $prestation){
-			array_push($events, Event::findOne(['idevent' => $prestation->idevent]));
-			array_push($jurys, Jury::findOne(['idjury' => $prestation->idjury]));
-		}
-
-		$this->render("index", array('results' => $prestations, 'events' => $events, 'jury' => $jurys));
-	}
-
-
-	public function view(){
-		try {
-			$prestation = new Prestation(parameters()["id"]);
-            $individual_evaluation = IndividualEvaluation::findOne(['idprestation' => intval(parameters()["id"])]);
-
-            $jury = Jury::findOne(['idjury' => $prestation->idjury]);
-
-            $this->render("view", array('prestation' => $prestation, 'individual_evaluation' => $individual_evaluation, 'criteres' => EvaluationCriteria::findAll()));
-        } catch (Exception $e) {
-            (new SiteController())->render("index");
+        $user = new InternalUser($_SESSION["user"]["idinternaluser"]);
+        if (is_student()) {
+            $student = Student::findOne(["idinternaluser" => $user->idinternaluser])[0];
+            $this->student($student);
+        } else if (is_teacher() || is_admin()) {
+            $this->corrector($user);
+        } else {
+            go_back();
         }
-	}
+    }
+
+    public function student($student){
+    	$prestations = Prestation::findOne(["idstudent" => $student->idinternaluser->idinternaluser]);
+
+        $prestations = array_filter($prestations, function($prestation) {
+            return strtotime($prestation->date_prestation) < strtotime(date("Y-m-d H:i:s"));
+        });
+
+        $table_header = array("Evenement", "Eleve", "Salle", "Jury", "Date");
+
+        $table_content = array();
+        foreach ($prestations as &$prestation) {
+            $table_content[$prestation->idprestation] = array(
+                "Evenement" => $prestation->idevent->entitled_event,
+                "Eleve" => $prestation->idstudent->idinternaluser->nom." ".$prestation->idstudent->idinternaluser->prenom,
+                "Salle" => $prestation->idjury->idclassroom->name_classroom,
+                "Jury" => $prestation->idjury->name_jury,
+                "Date" => date_format(date_create($prestation->date_prestation),'d/m/y'). " de " . $prestation->start_time . " à " . $prestation->end_time
+            );
+        }
+
+        $this->renderComponent("table", ["header" => $table_header, "content" => $table_content]);
+    }
+
+    public function corrector($user){
+    	print_r("resultat corrector");
+    }
+
+
+
+	
 }
 
