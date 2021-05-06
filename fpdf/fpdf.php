3910b0e26720ad2430bf3384db11dbf5f06d4601
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-05-07 01:40:10 +0200

    set utf-8 in fpdf

--- fpdf/fpdf.php
+++ fpdf/fpdf.php
@@ -623,6 +623,7 @@ function Cell($w, $h=0, $txt='', $border=0, $ln=0, $align='', $fill=false, $link
 	{
 		if(!isset($this->CurrentFont))
 			$this->Error('No font has been set');
+		$txt = iconv('utf-8', 'cp1252', $txt);
 		if($align=='R')
 			$dx = $w-$this->cMargin-$this->GetStringWidth($txt);
 		elseif($align=='C')
