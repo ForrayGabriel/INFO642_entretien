af20a9b48859311926968a4a88df2c35e37306d2
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-05-06 23:49:53 +0200

    bug fix contact display name

--- view/contact/view.php
+++ view/contact/view.php
@@ -11,40 +11,40 @@
 
 
 <?php
-// Need to be remove (Dorian)
-foreach($data['internaluser'] as $user){
-	if($data['contact']->idinternaluser_requestor->idinternaluser == $user->idinternaluser){
-		$display_name_requestor = $user->nom . " " . $user->prenom;
-		$id_requestor = $user->idinternaluser;
- 	}else if($data['contact']->idinternaluser_receiver->idinternaluser == $user->idinternaluser){
-		$display_name_receiver = $user->nom . " " . $user->prenom;
-		$id_receiver = $user->idinternaluser;
- 	}
+if($data['contact']->idinternaluser_requestor->idinternaluser == get_id()){
+	// REQUESTOR
+	echo "<div class='container'>";
+	echo "<img src='https://latelierduformateur.fr/wp-content/uploads/2018/03/avatar-1606916_960_720.png' alt='Avatar'>";
+	echo "<p><b>De " . $data['contact']->idinternaluser_requestor->nom . " " . $data['contact']->idinternaluser_requestor->prenom ."  : ". $data['contact']->title_contact ."  </b> </br></br> ". $data['contact']->description_contact ."</p>";
+	echo "<span class='time-right'>". date_format(date_create($data['contact']->date_contact),'d-m-Y H:i:s') ."</span>";
+	echo "</div>";
+}else{
+	//RECEIVER
+	echo "<div class='container darker'>";
+	echo "<img src='http://judowormhout.ovh/wp-content/uploads/2016/06/avatar7.png' alt='Avatar' class='right'>";
+	echo "<p><b>De " . $data['contact']->idinternaluser_requestor->nom . " " . $data['contact']->idinternaluser_requestor->prenom ."  : ". $data['contact']->title_contact ."  </b> </br></br> ". $data['contact']->description_contact ."</p>";
+	echo "<span class='time-left'>". date_format(date_create($data['contact']->date_contact),'d-m-Y H:i:s') ."</span>";
+	echo "</div>";
 }
 
-echo "<div class='container'>";
-echo "<img src='https://latelierduformateur.fr/wp-content/uploads/2018/03/avatar-1606916_960_720.png' alt='Avatar'>";
-echo "<p><b>De " . $display_name_requestor . "  : ". $data['contact']->title_contact ."  </b> </br></br> ". $data['contact']->description_contact ."</p>";
-echo "<span class='time-right'>". date_format(date_create($data['contact']->date_contact),'Y-m-d H:i:s') ."</span>";
-echo "</div>";
 
 
 foreach($data['response'] as $response){
-	if($data['contact']->idusercontact == $response->idusercontact->idusercontact){
-		if($response->idinternaluser_requestor->idinternaluser == $id_requestor){
-			echo "<div class='container'>";
-			echo "<img src='https://latelierduformateur.fr/wp-content/uploads/2018/03/avatar-1606916_960_720.png' alt='Avatar'>";
-			echo "<p><b>De " . $response->idinternaluser_requestor->nom . " " . $response->idinternaluser_requestor->prenom . "  : ". $response->title_response ."  </b> </br></br> ". $response->text_response ."</p>";
-			echo "<span class='time-right'>". date_format(date_create($response->date_response),'Y-m-d H:i:s') ."</span>";
-			echo "</br>";
-			echo "</div>";
-		}else{
-			echo "<div class='container darker'>";
-			echo "<img src='http://judowormhout.ovh/wp-content/uploads/2016/06/avatar7.png' alt='Avatar' class='right'>";
-			echo "<p><b>De " . $response->idinternaluser_receiver->nom . " " . $response->idinternaluser_receiver->prenom . "  : ". $response->title_response ."  </b> </br></br> ". $response->text_response ."</p>";
-			echo "<span class='time-left'>". date_format(date_create($response->date_response),'Y-m-d H:i:s') ."</span>";
-			echo "</div>";
-		}
+	if($response->idinternaluser_requestor->idinternaluser == get_id()){
+		// REQUESTOR
+		echo "<div class='container'>";
+		echo "<img src='https://latelierduformateur.fr/wp-content/uploads/2018/03/avatar-1606916_960_720.png' alt='Avatar'>";
+		echo "<p><b>De " . $response->idinternaluser_requestor->nom . " " . $response->idinternaluser_requestor->prenom . "  : ". $response->title_response ."  </b> </br></br> ". $response->text_response ."</p>";
+		echo "<span class='time-right'>". date_format(date_create($response->date_response),'d-m-Y H:i:s') ."</span>";
+		echo "</br>";
+		echo "</div>";
+	}else{
+		// RECEIVER
+		echo "<div class='container darker'>";
+		echo "<img src='http://judowormhout.ovh/wp-content/uploads/2016/06/avatar7.png' alt='Avatar' class='right'>";
+		echo "<p><b>De " . $response->idinternaluser_requestor->nom . " " . $response->idinternaluser_requestor->prenom . "  : ". $response->title_response ."  </b> </br></br> ". $response->text_response ."</p>";
+		echo "<span class='time-left'>". date_format(date_create($response->date_response),'d-m-Y H:i:s') ."</span>";
+		echo "</div>";
 	}
 }
 
@@ -58,9 +58,8 @@ foreach($data['response'] as $response){
 
 <div class='form-container'>
 	<form action='?r=contact/reply' method='post'>
-
-		<input type="hidden" name="answer_iduser_requestor" value=<?php echo $id_requestor;?> >
-		<input type="hidden" name="answer_iduser_receiver" value=<?php echo $id_receiver; ?> >
+		<input type="hidden" name="answer_iduser_requestor" value=<?php echo $data['contact']->idinternaluser_requestor->idinternaluser; ?> >	
+		<input type="hidden" name="answer_iduser_receiver" value=<?php echo $data['contact']->idinternaluser_receiver->idinternaluser; ?> >
 		<input type="hidden" name="answer_idcontact" value=<?php echo $data['contact']->idusercontact; ?> >
 
 		<div class='form-group'>
