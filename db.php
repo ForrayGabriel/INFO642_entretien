d33acd15b9817f91b36d820c4b20d1799cc7c95e
clement-massit <clement.massit@gmail.com>
2021-05-07 12:01:27 +0200

    UTF8 corrected

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 // Modifier la dbname (votre login), login et mot de passe
 
 $db = new PDO("mysql:host=localhost;dbname=info642","root","");
-
+$db->exec("SET NAMES 'utf8';");
 
 
 function db() { global $db; return $db; }
